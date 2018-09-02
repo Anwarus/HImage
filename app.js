57a4991b844323e560d44026dc07f42908562ffe
Anwarus <Anwarus1289@gmail.com>
2018-09-02 11:49:15 +0200

    changed vars names

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
 import $ from 'jquery';
 
+//Pass custom button click to image input
 $("#image").click(() => {
     $('input:file')[0].click();
 });
+  
+var imageInput = document.getElementById("image-input");
+var imageContainer = document.getElementById("image-container");
+var canvas = document.getElementById("canvas");
 
-function showImage(src,image,canvas) {
+showImage(imageInput, imageContainer, canvas);
+
+function showImage(imageInput,imageContainer,canvas) {
     let fileReader = new FileReader();
 
     //When image is loaded, set the src of the image where you want to display it
     fileReader.onload = (e) => {
-        image.src = e.target.result;
+        imageContainer.src = e.target.result;
 
         canvas.width = 1920;
         canvas.height = 1080;
-        canvas.getContext('2d').drawImage(image, 0, 0, image.naturalWidth, image.naturalHeight);
+        canvas.getContext('2d').drawImage(imageContainer, 0, 0, imageContainer.naturalWidth, imageContainer.naturalHeight);
         getAverageColor(canvas);
         getMostUsedColors(canvas, 4);
     }
     
-    src.addEventListener("change",function() {
+    imageInput.addEventListener("change",function() {
       //Fill fr with image data    
       fileReader.readAsDataURL(src.files[0]);
     });
   }
-  
-var src = document.getElementById("src");
-var image = document.getElementById("target");
-var canvas = document.getElementById("canvas");
-
-showImage(src,image,canvas);
 
 function getAverageColor(canvas) {
     let pixels = canvas.getContext('2d').getImageData(0, 0, canvas.width, canvas.height);
@@ -69,9 +70,6 @@ function getMostUsedColors(canvas, numberOfColors) {
     //Associative array where we will used colors and count of use times
     let colors = {};
 
-    let mostUsedColors = Array.from({ length: numberOfColors }, () => 0);
-    let mostUsedRGBColors = Array.from({ length: numberOfColors }, () => 0);
-
     for(let i=0; i<pixelColors.length; i+=128) {
         //Normalize colors to suit our scale
         let r = Math.floor(pixelColors[i] * normalizedMaxValue / 255.0).toString();
@@ -96,22 +94,16 @@ function getMostUsedColors(canvas, numberOfColors) {
     });
 
     colors = colors.slice(0, numberOfColors);
-
-    // for(let i=0; i<mostUsedColors.length; i++) {
-    //     let mostUsedColor = mostUsedColors[i].toString();
-
-    //     if(mostUsedColor.length == 4)
-    //         mostUsedColor = '00' + mostUsedColor;
-
-    //     if(mostUsedColor.length == 5)
-    //         mostUsedColor = '0' + mostUsedColor;
-
-    //     mostUsedRGBColors[i] = {
-    //         r: denormalize(parseInt(mostUsedColor.substring(0, 2)), normalizedMaxValue),
-    //         g: denormalize(parseInt(mostUsedColor.substring(2, 4)), normalizedMaxValue),
-    //         b: denormalize(parseInt(mostUsedColor.substring(4, 6)), normalizedMaxValue)
-    //     };
-    // }
+    console.log(colors);
+    let mostUsedRGBColors = [];
+    for(let i=0; i<numberOfColors; i++) {
+        mostUsedRGBColors[i] = {
+            r: denormalize(parseInt(colors[i].substring(0, 2))),
+            g: denormalize(parseInt(colors[i].substring(2, 4))),
+            b: denormalize(parseInt(colors[i].substring(4, 6)))
+        };
+    }
+    console.log(mostUsedRGBColors);
 
     $('#first-left-color').css({ fill: "rgba(" + mostUsedRGBColors[0].r + ", " + mostUsedRGBColors[0].g + ", " + mostUsedRGBColors[0].b + ", 255)" });
     $('#second-left-color').css({ fill: "rgba(" + mostUsedRGBColors[1].r + ", " + mostUsedRGBColors[1].g + ", " + mostUsedRGBColors[1].b + ", 255)" });
@@ -119,8 +111,12 @@ function getMostUsedColors(canvas, numberOfColors) {
     $('#second-right-color').css({ fill: "rgba(" + mostUsedRGBColors[3].r + ", " + mostUsedRGBColors[3].g + ", " + mostUsedRGBColors[3].b + ", 255)" });
 }
 
-function denormalize(value, factor) {
-    return Math.floor((value * 255) / factor);
+function denormalize(value) {
+    return Math.floor((value * 255) / 31);
+}
+
+function normalize (value) {
+    return Math.floor((value * 31) / 255);
 }
 
 function addLeadingZero(colorPart) {
