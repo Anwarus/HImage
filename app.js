312826950a7208f0a90b11279b8b46096eeaf41d
Anwarus <Anwarus1289@gmail.com>
2018-09-17 20:20:41 +0200

    style after image selected

--- app.js
+++ app.js
@@ -1,122 +1,53 @@
 import $ from 'jquery';
+import { Utils } from './Utils';
 
-//Pass custom button click to image input
-$("#image").click(() => {
-    $('input:file')[0].click();
-});
-  
-var imageInput = document.getElementById("image-input");
-var imageContainer = document.getElementById("image-container");
-var canvas = document.getElementById("canvas");
-
-$(imageInput).on('change', () => {
-    let fileReader = new FileReader();
-    console.log('test');
-    //When image is loaded, set the src of the image where you want to display it
-    fileReader.onload = (e) => {
-        imageContainer.src = e.target.result;
-
-        canvas.width = 1920;
-        canvas.height = 1080;
-        canvas.getContext('2d').drawImage(imageContainer, 0, 0, imageContainer.naturalWidth, imageContainer.naturalHeight);
-        getAverageColor(canvas);
-        getMostUsedColors(canvas, 4);
-    }
-
-    fileReader.readAsDataURL(imageInput.files[0]);
-});
-
-function getAverageColor(canvas) {
-    let pixels = canvas.getContext('2d').getImageData(0, 0, canvas.width, canvas.height);
-    let pixelColors = pixels.data;
-    let pixelCount = pixels.width * pixels.height;
+$(function() {
+    //Pass custom button click to image input
+    $("#select-image").click(() => {
+        $('input:file')[0].click();
+    });
     
-    let rSum = 0;
-    let gSum = 0;
-    let bSum = 0;
+    var imageInput = document.getElementById("image-input");
+    var imageContainer = document.getElementById("image-container");
+    var canvas = document.getElementById("canvas");
 
-    for(let i=0; i<pixelColors.length; i+=4) {
-        rSum += pixelColors[i];
-        gSum += pixelColors[i+1];
-        bSum += pixelColors[i+2];
-    }
-
-    let result = {
-        r: Math.floor(rSum/pixelCount),
-        g: Math.floor(gSum/pixelCount),
-        b: Math.floor(bSum/pixelCount)
-    };
-
-    $('#main-color').css({ fill: "rgba(" + result.r + ", " + result.g + ", " + result.b + ", 255)" });
-    $('#first-left-color').css({ fill: "rgba(" + 168 - result.r + ", " + 168 - result.g + ", " + 168 - result.b + ", 255)" });
-    $('#second-left-color').css({ fill: "rgba(" + 1 + result.r + ", " + 1 + result.g + ", " + 1 + result.b + ", 255)" });
-    $('#first-right-color').css({ fill: "rgba(" + 255 - result.r + ", " + 255 - result.g + ", " + 255 - result.b + ", 255)" });
-    $('#second-right-color').css({ fill: "rgba(" + 255 + result.r + ", " + 255 + result.g + ", " + 255 + result.b + ", 255)" });
-}
-
-function getMostUsedColors(canvas, numberOfColors) {
-    let pixels = canvas.getContext('2d').getImageData(0, 0, canvas.width, canvas.height);
-    let pixelColors = pixels.data;
-    let pixelCount = pixels.width * pixels.height;
-
-    //We want to convert colors from 0-255 to 0-31 scale
-    let normalizedMaxValue = 31;
-
-    //Associative array where we will used colors and count of use times
-    let colors = {};
-
-    for(let i=0; i<pixelColors.length; i+=128) {
-        //Normalize colors to suit our scale
-        let r = Math.floor(pixelColors[i] * normalizedMaxValue / 255.0).toString();
-        let g = Math.floor(pixelColors[i+1] * normalizedMaxValue / 255.0).toString();
-        let b = Math.floor(pixelColors[i+2] * normalizedMaxValue / 255.0).toString();
+    //On image change draw image in canvas using FileReader to get pixel data
+    $(imageInput).on('change', () => {
+        let fileReader = new FileReader();
         
-        r = addLeadingZero(r);
-        g = addLeadingZero(g);
-        b = addLeadingZero(b);
+        //On image load by FileReader
+        fileReader.onload = (e) => {
+            imageContainer.src = e.target.result;
 
-        //Make from color parts key
-        let key = r + g + b;
+            //Configure canvas and draw image
+            canvas.width = imageContainer.naturalWidth;
+            canvas.height = imageContainer.naturalHeight;
+            canvas.getContext('2d').drawImage(imageContainer, 0, 0, imageContainer.naturalWidth, imageContainer.naturalHeight);
+            
+            //Extract pixels object data
+            let pixels = canvas.getContext('2d').getImageData(0, 0, canvas.width, canvas.height);
 
-        if(colors[key] === undefined)
-            colors[key] = 0;
-        else
-            colors[key] ++;
-    }
+            let averageColor = Utils.getAverageColor(pixels);
+            let mostUsedColors = Utils.getMostUsedColors(pixels, 4);
 
-    colors = Object.keys(colors).sort((a, b) => {
-        return colors[a] - colors[b];
+            $('#main-color').css({ fill: "rgba(" + averageColor.r + ", " + averageColor.g + ", " + averageColor.b + ", 255)" });
+            $('#first-left-color').css({ fill: "rgba(" + mostUsedColors[0].r + ", " + mostUsedColors[0].g + ", " + mostUsedColors[0].b + ", 255)" });
+            $('#second-left-color').css({ fill: "rgba(" + mostUsedColors[1].r + ", " + mostUsedColors[1].g + ", " + mostUsedColors[1].b + ", 255)" });
+            $('#first-right-color').css({ fill: "rgba(" + mostUsedColors[2].r + ", " + mostUsedColors[2].g + ", " + mostUsedColors[2].b + ", 255)" });
+            $('#second-right-color').css({ fill: "rgba(" + mostUsedColors[3].r + ", " + mostUsedColors[3].g + ", " + mostUsedColors[3].b + ", 255)" });
+
+            //Move logo polygon up to make more space for image
+            $('#logo').toggleClass('slide-up');
+            //Move decoration polygon up to make more space for image
+            $('#decoration').toggleClass('slide-up');
+            //Move color polygons up to make more space for image
+            $('#colors').toggleClass('slide-up');
+            //Move image select polygon down to make more space for image
+            $('#select-image').toggleClass('slide-down');
+        }
+
+        //Load image to FileReader
+        fileReader.readAsDataURL(imageInput.files[0]);
     });
 
-    colors = colors.slice(0, numberOfColors);
-    console.log(colors);
-    let mostUsedRGBColors = [];
-    for(let i=0; i<numberOfColors; i++) {
-        mostUsedRGBColors[i] = {
-            r: denormalize(parseInt(colors[i].substring(0, 2))),
-            g: denormalize(parseInt(colors[i].substring(2, 4))),
-            b: denormalize(parseInt(colors[i].substring(4, 6)))
-        };
-    }
-    console.log(mostUsedRGBColors);
-
-    $('#first-left-color').css({ fill: "rgba(" + mostUsedRGBColors[0].r + ", " + mostUsedRGBColors[0].g + ", " + mostUsedRGBColors[0].b + ", 255)" });
-    $('#second-left-color').css({ fill: "rgba(" + mostUsedRGBColors[1].r + ", " + mostUsedRGBColors[1].g + ", " + mostUsedRGBColors[1].b + ", 255)" });
-    $('#first-right-color').css({ fill: "rgba(" + mostUsedRGBColors[2].r + ", " + mostUsedRGBColors[2].g + ", " + mostUsedRGBColors[2].b + ", 255)" });
-    $('#second-right-color').css({ fill: "rgba(" + mostUsedRGBColors[3].r + ", " + mostUsedRGBColors[3].g + ", " + mostUsedRGBColors[3].b + ", 255)" });
-}
-
-function denormalize(value) {
-    return Math.floor((value * 255) / 31);
-}
-
-function normalize (value) {
-    return Math.floor((value * 31) / 255);
-}
-
-function addLeadingZero(colorPart) {
-    if(colorPart.length === 1)
-        colorPart = '0' + colorPart;
-    
-    return colorPart;
-}
+});
