d04361b14a3900658c1d6be7f0707c46a55c23bc
Anwarus <Anwarus1289@gmail.com>
2018-09-18 20:15:26 +0200

    animations for slide down and up

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 import $ from 'jquery';
+import jQuery from 'jquery';
 import { Utils } from './Utils';
+
+jQuery.fn.extend({
+    replaceClass: function(oldClass, newClass, force = false) {
+        if($(this).hasClass(oldClass) || force === true)
+            $(this).removeClass(oldClass).addClass(newClass);
+    }
+});
 
 $(function() {
     //Pass custom button click to image input
@@ -14,6 +22,11 @@ $(function() {
     //On image change draw image in canvas using FileReader to get pixel data
     $(imageInput).on('change', () => {
         let fileReader = new FileReader();
+
+        $('#logo').replaceClass('slide-up', 'slide-down');
+        $('#decoration').replaceClass('slide-up', 'slide-down');
+        $('#colors').replaceClass('slide-up', 'slide-down');
+        $('#select-image').replaceClass('slide-down', 'slide-up');
 
         //On image load by FileReader
         fileReader.onload = (e) => {
@@ -39,13 +52,13 @@ $(function() {
                 $('#second-right-color').css({ fill: "rgba(" + mostUsedColors[3].r + ", " + mostUsedColors[3].g + ", " + mostUsedColors[3].b + ", 255)" });
 
                 //Move logo polygon up to make more space for image
-                $('#logo').toggleClass('slide-up');
+                $('#logo').replaceClass('slide-down', 'slide-up', true);
                 //Move decoration polygon up to make more space for image
-                $('#decoration').toggleClass('slide-up');
+                $('#decoration').replaceClass('slide-down', 'slide-up', true);
                 //Move color polygons up to make more space for image
-                $('#colors').toggleClass('slide-up');
+                $('#colors').replaceClass('slide-down', 'slide-up', true);
                 //Move image select polygon down to make more space for image
-                $('#select-image').toggleClass('slide-down');
+                $('#select-image').replaceClass('slide-up', 'slide-down', true);
             };
         }
 
